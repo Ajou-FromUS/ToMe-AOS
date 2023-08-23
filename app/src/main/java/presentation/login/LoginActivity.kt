763967126e22716a2d1ada6a2a6743177a1c0ff4
Jong <jm991014@gmail.com>
2023-08-23 17:24:34 +0900

    [feat] 로그인 페이지 구현
    액티비티 및 레이아웃 구현, 버튼 에셋 추가

---
+++ app/src/main/java/presentation/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package presentation.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tome_aos.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
