70e24e5f0e232fa3a6a3c88318b4a24eaf991d8d
Jong <jm991014@gmail.com>
2023-08-23 17:24:06 +0900

    [chore] manifest 및 landingpage 수정
    color 일부 추가, string 추가, 버튼 asset 추가 외 이미지 제외 랜딩 페이지 구현 완료

--- app/src/main/java/presentation/landing/FifthFragment.kt
+++ app/src/main/java/presentation/landing/FifthFragment.kt
@@ -1,18 +1,25 @@
 package presentation.landing
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.tome_aos.R
+import com.example.tome_aos.databinding.FragmentFifthBinding
+import presentation.login.LoginActivity
 
 class FifthFragment : Fragment() {
-
+    private lateinit var binding: FragmentFifthBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_fifth, container, false)
+        binding = FragmentFifthBinding.inflate(inflater, container, false)
+        binding.btnStartTome.setOnClickListener{
+            var intent = Intent(activity, LoginActivity::class.java)
+            startActivity(intent)
+        }
+        return binding.root
     }
 }
