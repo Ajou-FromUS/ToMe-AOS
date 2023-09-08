17382ecb1617ccde8175c2bfa970f70b802c8b7e
Jong <jm991014@gmail.com>
2023-09-08 22:35:47 +0900

    [fix] 슬라이드 시 textview 사라지는 기능 수정
    및 레이아웃 id 네이밍 규칙 일치

--- app/src/main/java/presentation/landing/LandingActiviy.kt
+++ app/src/main/java/presentation/landing/LandingActiviy.kt
@@ -3,26 +3,40 @@ package presentation.landing
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import com.example.tome_aos.databinding.ActivityLandingBinding
 
 class LandingActiviy : AppCompatActivity() {
+    private lateinit var binding: ActivityLandingBinding
+    private lateinit var viewPager: ViewPager2
+    private lateinit var slideText: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
-        val binding = ActivityLandingBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
+        binding = ActivityLandingBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val viewPagerAdapter = ViewpagerFragmentAdapter(this)
-        binding.landingViewpager.adapter = viewPagerAdapter
-        binding.dotIndicator.attachTo(binding.landingViewpager)
+        viewPager = binding.vpLanding
+        slideText = binding.tvSlide
 
-//        binding.btnBack.setOnClickListener {
-//            startActivity(Intent(this, LandingActivity::class.java))
-//            finish()
-//        }
-        //뒤로가기 버튼 입력 시, 랜딩페이지 0번째 페이지로 이동
+        val viewPagerAdapter = ViewpagerFragmentAdapter(this)
+        viewPager.adapter = viewPagerAdapter
+        binding.dotIndicator.attachTo(viewPager)
+
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                if (position == 4) {
+                    slideText.visibility = View.GONE
+                } else {
+                    slideText.visibility = View.VISIBLE
+                }
+            }
+        })
     }
 
     private inner class ViewpagerFragmentAdapter(fragmentActivity: FragmentActivity) :
