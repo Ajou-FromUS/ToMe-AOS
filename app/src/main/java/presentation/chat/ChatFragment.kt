9467d6e422a524af2581a618213f9fe07ef3b156
Jong <jm991014@gmail.com>
2023-09-24 18:10:59 +0900

    [feat] chat 일부 구현 완료

--- app/src/main/java/presentation/chat/ChatFragment.kt
+++ app/src/main/java/presentation/chat/ChatFragment.kt
@@ -1,10 +1,12 @@
 package presentation.chat
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import com.example.tome_aos.R
 import com.example.tome_aos.databinding.FragmentChatBinding
 
@@ -15,7 +17,26 @@ class ChatFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentChatBinding.inflate(inflater, container, false)
-
-        return inflater.inflate(R.layout.fragment_chat, container, false)
+        var instVisibility = binding.tvChatInst.isVisible
+        val ibtnChatQuestion = binding.ibtnChatQuestion
+        val btnStartChat = binding.btnStartChat
+        ibtnChatQuestion.setOnClickListener {
+            if(!instVisibility) {
+                instVisibility = true
+            }else{
+                instVisibility = false
+            }
+        }
+        btnStartChat.setOnClickListener {
+            val fragment = ChatTalk()
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.frame_chat, fragment, "chatTalk")
+                .commit()
+            //어플리케이션 종료 전까지는 계속 1단계 화면에 남아있어야 하기에 replace
+        }
+        binding.root.setOnClickListener {
+            instVisibility = false
+        }
+        return binding.root
     }
 }
