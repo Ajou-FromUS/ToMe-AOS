1bf9c6a8efb836f97373c4014c31110ddb618730
MinJu Jo <pida6716@gmail.com>
2023-11-07 18:00:35 +0900

    [add] mission detail page

---
+++ app/src/main/java/presentation/mission/MissionDetailFragment.kt
@@ -0,0 +1,21 @@
+package presentation.mission
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.tome_aos.R
+
+
+class MissionDetailFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_mission_detail, container, false)
+    }
+
+}
