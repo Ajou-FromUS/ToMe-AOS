617534a5e0cff92e57d82f7037f7b335ead96168
Jong <jm991014@gmail.com>
2023-11-26 16:12:02 +0900

    [add] 스플래시 anim, asset 추가 및 적용

--- app/src/main/java/presentation/splash/SplashActivity.kt
+++ app/src/main/java/presentation/splash/SplashActivity.kt
@@ -5,20 +5,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.animation.AnimationUtils
+import com.example.tome_aos.R
 import com.example.tome_aos.databinding.ActivitySplashBinding
 import presentation.landing.LandingActiviy
 
 class SplashActivity : AppCompatActivity() {
     private val handler = Handler(Looper.getMainLooper())
+    private lateinit var binding: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivitySplashBinding.inflate(layoutInflater)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         moveNext()
     }
 
     private fun moveNext(){
+        binding.ivFaceLogo.startAnimation(AnimationUtils.loadAnimation(this, R.anim.slide_left))
+        binding.tvSplash.startAnimation(AnimationUtils.loadAnimation(this, R.anim.slide_right))
+        binding.ivTextLogo.startAnimation(AnimationUtils.loadAnimation(this, R.anim.slide_right))
         handler.postDelayed({
             val intent = Intent(this, LandingActiviy::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
