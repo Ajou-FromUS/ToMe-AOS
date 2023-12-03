82f8546ac39c704b21fc4776df1c43e1eaa5377f
MinJu Jo <pida6716@gmail.com>
2023-12-04 02:48:11 +0900

    [refactor] image retrofit

--- app/src/main/java/presentation/mission/photo/MissionPhotoFragment.kt
+++ app/src/main/java/presentation/mission/photo/MissionPhotoFragment.kt
@@ -1,7 +1,9 @@
 package presentation.mission.photo
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.app.Activity
+import android.content.ContentResolver
 import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
@@ -9,9 +11,12 @@ import android.content.pm.PackageManager
 import android.database.Cursor
 import android.graphics.Bitmap
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
+import android.provider.OpenableColumns
+import android.provider.Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -23,37 +28,36 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import application.ApplicationClass
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CircleCrop
-import com.canhub.cropper.CropImage.CancelledResult.bitmap
 import com.canhub.cropper.CropImageContract
-import com.canhub.cropper.CropImageView
-import com.canhub.cropper.CropImageView.Guidelines
+import com.example.tome_aos.R
 import com.example.tome_aos.databinding.FragmentMissionPhotoBinding
-import com.google.gson.GsonBuilder
-import data.dto.request.MissionCompleteRequest
-import data.dto.request.MissionImageRequest
 import data.dto.response.MissionResponse
 import data.service.ApiClient
-import data.service.MissionCompleteService
+import data.service.MissionImageService
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
-import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import okhttp3.internal.http.hasBody
+import okio.BufferedSink
+import okio.source
 import presentation.MainActivity
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.http.Multipart
 import java.io.File
+import java.io.FileOutputStream
 import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
@@ -65,17 +69,16 @@ class MissionPhotoFragment : Fragment() {
     private lateinit var againButton: Button
     private lateinit var missionImage: ImageView
     private lateinit var detailText: TextView
+//    private lateinit var baseLayout:
 
     lateinit var bitmap: Bitmap
 
-    private lateinit var currentPhotoPath: String
-    private var getPhotoURI: Uri? = null
+    private lateinit var currentPhotoPath: Uri
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         binding = FragmentMissionPhotoBinding.inflate(inflater, container, false).apply {
             showButton = showMissionPhoto
             againButton = chooseAgainBtn
@@ -88,19 +91,46 @@ class MissionPhotoFragment : Fragment() {
 
         detailText.text = missionTitle
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            if (!Environment.isExternalStorageManager()) {
+                val intent = Intent(ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION)
+                startActivity(intent)
+            }
+        }
+
         // 권한 부여 여부
         val isEmpower = ContextCompat.checkSelfPermission(context as MainActivity,
             Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(context as MainActivity,
-            Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+            Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(context as MainActivity,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
 
+        // 현재 기기에 설정된 쓰기 권한을 가져오기 위한 변수
+        var writePermission = ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE )
+        // 현재 기기에 설정된 읽기 권한을 가져오기 위한 변수
+        var readPermission = ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE)
 
+        chooseImage()
+
+        showButton.setOnClickListener {
+            Log.d("Image uri", currentPhotoPath.toString())
+            if(currentPhotoPath != null){
+                patchImageMission(currentPhotoPath, missionID)
+            }
+        }
+
+        againButton.setOnClickListener {
+            chooseImage()
+        }
+
+        return binding.root
+    }
+
+    private fun chooseImage(){
         var chooseOne: Array<String> = arrayOf("카메라로 촬영하기", "앨범에서 선택하기") // 리스트에 들어갈 Array
-
         val builder = context?.let { AlertDialog.Builder(it) }
         builder?.setTitle("두 가지 방법 중 고르기!")?.setItems(chooseOne,
             DialogInterface.OnClickListener { dialog, which ->
                 if (which == 0){ // 카메라
-                    startCapture()
                     val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
                     callCamera.launch(intent)
                 }else{ // 앨범
@@ -108,23 +138,18 @@ class MissionPhotoFragment : Fragment() {
                     intent.type = "image/*"
                     intent.putExtra("crop", true)
                     callAlbum.launch(intent)
-                    //openGalleryForImage()
-                    // 권한 부여 되지 않았을경우
-//                    if (isEmpower) {
-//                        empowerPhotoandReadStorage()
-//                        // 권한 부여 되었을 경우
-//                    } else {
-//                        val intent = Intent(Intent.ACTION_PICK)
-//                        intent.type = "image/*"
-//                        callAlbum.launch(intent)
-//                    }
                 }
             })
-        // 다이얼로그를 띄워주기
         builder?.show()
-
-        return binding.root
     }
+
+    @SuppressLint("ResourceAsColor")
+    private fun imageError(){
+        showButton.visibility = View.INVISIBLE
+        againButton.text = "사진 다시 선택하기"
+        againButton.background.setTint(R.color.color_main)
+    }
+
 
     private val cropImage = registerForActivityResult(CropImageContract()) { result ->
         if (result.isSuccessful) {
@@ -174,72 +199,52 @@ class MissionPhotoFragment : Fragment() {
 //            .start(this)
 //    }
 
-    private fun openGalleryForImage() {
-        Intent(Intent.ACTION_PICK).apply {
-            putExtra("crop", true)
-            type = MediaStore.Images.Media.CONTENT_TYPE
-            choiceAlbumLauncher.launch(this)
-        }
 
-    }
+//    private fun startCapture() {
+//        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+//            takePictureIntent.resolveActivity(requireActivity().packageManager)?.also {
+//                val photoFile: File? = try {
+//                    createImageFile()
+//                } catch (ex: IOException) {
+//                    null
+//                }
+//                photoFile?.also {
+//                    val photoURI: Uri = FileProvider.getUriForFile(
+//                        requireActivity(),
+//                        "com.example.tome_aos.file-provider",
+//                        it
+//                    )
+//                    getPhotoURI = photoURI
+//
+//                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+//                    takePictureLauncher.launch(takePictureIntent)
+//                }
+//            }
+//        }
+//    }
 
-    private fun startCapture() {
-        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
-            takePictureIntent.resolveActivity(requireActivity().packageManager)?.also {
-                val photoFile: File? = try {
-                    createImageFile()
-                } catch (ex: IOException) {
-                    null
-                }
-                photoFile?.also {
-                    val photoURI: Uri = FileProvider.getUriForFile(
-                        requireActivity(),
-                        "com.example.tome_aos.file-provider",
-                        it
-                    )
-                    getPhotoURI = photoURI
-
-                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
-                    takePictureLauncher.launch(takePictureIntent)
-                }
-            }
-        }
-    }
-
-    @Throws(IOException::class)
-    private fun createImageFile(): File {
-        Log.d("image", "createImageFile")
-        val timeStamp: String = SimpleDateFormat().format(Date())
-        val storageDir: File? =
-            requireActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-        return File.createTempFile(
-            "JPEG_${timeStamp}_",
-            ".jpg",
-            storageDir
-        ).apply {
-            currentPhotoPath = absolutePath
-        }
-    }
+//    @Throws(IOException::class)
+//    private fun createImageFile(): File {
+//        Log.d("image", "createImageFile")
+//        val timeStamp: String = SimpleDateFormat().format(Date())
+//        val storageDir: File? =
+//            requireActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+//        return File.createTempFile(
+//            "JPEG_${timeStamp}_",
+//            ".jpeg",
+//            storageDir
+//        ).apply {
+//            currentPhotoPath = absolutePath
+//        }
+//    }
 
     private val takePictureLauncher =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
             if (it.resultCode == Activity.RESULT_OK) {
-                getPhotoURI?.let { uri ->
+                currentPhotoPath?.let { uri ->
                     //cropSingleImage(uri)
                 }
                 return@registerForActivityResult
-            }
-        }
-
-    private val choiceAlbumLauncher =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-            it.data?.let { data ->
-                data.data?.let { dataUri ->
-                    Glide.with(this)
-                        .load(it)
-                        .transform(CircleCrop())
-                        .into(binding.detailImageView)
-                }
             }
         }
 
@@ -259,13 +264,13 @@ class MissionPhotoFragment : Fragment() {
 //        val file = File(mediaPath)
 //    }
 
-    private val cropPictureLauncher =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-            if (it.resultCode == Activity.RESULT_OK) {
-                val file = File(currentPhotoPath)// 사용하면 됨
-                return@registerForActivityResult
-            }
-        }
+//    private val cropPictureLauncher =
+//        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+//            if (it.resultCode == Activity.RESULT_OK) {
+//                val file = File(currentPhotoPath)// 사용하면 됨
+//                return@registerForActivityResult
+//            }
+//        }
 
 //    private fun cropSingleImage(photoUriPath: Uri) {
 //        val cropIntent = Intent(CROP_INTENT).apply {
@@ -302,19 +307,57 @@ class MissionPhotoFragment : Fragment() {
 //        cropPictureLauncher.launch(intent)
 //    }
 
-    // 레코딩, 파일 읽기 쓰기 권한부여
-//    private fun empowerPhotoandReadStorage(){
-//        val permissions = arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE)
-//        ActivityCompat.requestPermissions(context as MainActivity, permissions,0)
-//    }
-//
+
+    private fun getRealPathFromURI(uri: Uri, context: Context): String {
+        val contentResolver = context.contentResolver
+        val cursor = contentResolver.query(uri, null, null, null, null)
+        return if (cursor != null) {
+            val columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+            cursor.moveToFirst()
+            val filePath = cursor.getString(columnIndex)
+            cursor.close()
+            filePath
+        } else {
+            // Android 10 이상에서는 contentResolver.openInputStream 사용
+            val inputStream = contentResolver.openInputStream(uri)
+            val file = File(context.cacheDir, "tome_image.jpeg")
+            file.createNewFile()
+            inputStream?.use { input ->
+                FileOutputStream(file).use { output ->
+                    input.copyTo(output)
+                }
+            }
+            file.absolutePath
+        }
+    }
+
+    @SuppressLint("Range")
+    fun Uri.asMultipart(name: String, contentResolver: ContentResolver): MultipartBody.Part? {
+        return contentResolver.query(this, null, null, null, null)?.let {
+            if (it.moveToNext()) {
+                val displayName = it.getString(it.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                val requestBody = object : RequestBody() {
+                    override fun contentType(): MediaType? {
+                        return contentResolver.getType(this@asMultipart)?.toMediaType()
+                    }
+
+                    override fun writeTo(sink: BufferedSink) {
+                        sink.writeAll(contentResolver.openInputStream(this@asMultipart)?.source()!!)
+                    }
+                }
+                it.close()
+                MultipartBody.Part.createFormData("mission_image","image.jpeg", requestBody)
+            } else {
+                it.close()
+                null
+            }
+        }
+    }
+
     private val callAlbum: ActivityResultLauncher<Intent> = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
         if(it.resultCode == AppCompatActivity.RESULT_OK && it.data != null){
             val uri = it.data!!.data
-
-            val file = File(absolutelyPath(uri, requireContext()))
-            val requestFile = RequestBody.create("image/*".toMediaTypeOrNull(), file)
-            val body = MultipartBody.Part.createFormData("proFile", file.name, requestFile)
+            currentPhotoPath = uri!!
 
             Glide.with(this)
                 .load(uri)
@@ -324,44 +367,36 @@ class MissionPhotoFragment : Fragment() {
 
     private val callCamera: ActivityResultLauncher<Intent> = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
         if(it.resultCode == AppCompatActivity.RESULT_OK && it.data != null){
-            val extras = it.data!!.extras
+            val extras = it.data?.extras
             bitmap = extras?.get("data") as Bitmap
             binding.detailImageView.setImageBitmap(bitmap)
         }
     }
 
-    fun absolutelyPath(path: Uri?, context : Context): String {
-        var proj: Array<String> = arrayOf(MediaStore.Images.Media.DATA)
-        var c: Cursor? = context.contentResolver.query(path!!, proj, null, null, null)
-        var index = c?.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
-        c?.moveToFirst()
+    private fun patchImageMission(uri: Uri, missionID: Int?) {
+        val file = File(getRealPathFromURI(uri, requireContext()))
+        val requestFile = file.asRequestBody("image/*".toMediaTypeOrNull())
+        //val missionImage: MultipartBody.Part = MultipartBody.Part.createFormData("mission_image","image.jpeg", requestFile)
+        val missionImage: MultipartBody.Part = Uri.asMultipart()
 
-        var result = c?.getString(index!!)
+        Log.d("Image patch", missionImage.toString())
 
-        return result!!
-    }
-
-    private fun patchImageMission(content: MultipartBody.Part?, missionID: Int?) {
-        val client = ApiClient.getApiClient().create(MissionCompleteService::class.java)
+        val client = ApiClient.getApiClient().create(MissionImageService::class.java)
         lifecycleScope.launch {
             val accessToken = ApplicationClass.getInstance().getDataStore().accessToken.first()
             val refreshToken = ApplicationClass.getInstance().getDataStore().refreshToken.first()
-            val missionImageRequest = MissionImageRequest(content)
-            val requestBody = GsonBuilder()
-                .serializeNulls().create()
-                .toJson(missionImageRequest)
-                .toRequestBody("application/json".toMediaTypeOrNull())
-            client.getMissions(accessToken, refreshToken, missionID, requestBody).enqueue(object :
+            client.patchImageMissions(accessToken, refreshToken, missionID, missionImage).enqueue(object :
                 Callback<MissionResponse.Data> {
                 override fun onResponse(call: Call<MissionResponse.Data>, response: Response<MissionResponse.Data>) {
                     if (response.isSuccessful) {
+                        Log.d("Image success", response.body().toString())
                     } else {
-                        println("HTTP 오류: ${response.code()}")
+                        Log.d("Image else", "code: " + response.code().toString() + ", body: " + response.message())
+                        imageError()
                     }
                 }
                 override fun onFailure(call: Call<MissionResponse.Data>, t: Throwable) {
-                    Log.d("fail", t.toString())
-                    t.printStackTrace()
+                    Log.d("Image fail", t.toString())
                 }
             })
         }
