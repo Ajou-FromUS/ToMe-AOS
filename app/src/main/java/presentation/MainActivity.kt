8c85b616d38baa1d6688338e437153b18c468d9d
MinJu Jo <pida6716@gmail.com>
2023-12-06 04:15:47 +0900

    [fix] 메인 타이틀 수정

--- app/src/main/java/presentation/MainActivity.kt
+++ app/src/main/java/presentation/MainActivity.kt
@@ -1,12 +1,9 @@
 package presentation
 
-import android.app.Activity
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import application.ApplicationClass
@@ -17,22 +14,18 @@ import data.service.ApiClient
 import data.service.InitClient
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
-import presentation.chat.ChatActivity
 import presentation.home.HomeFragment
-import presentation.login.LoginWebviewActivity
 import presentation.mypage.MypageFragment
 import presentation.statistics.StatisticsFragment
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
     private val homeFragment = HomeFragment()
     private val mypageFragment = MypageFragment()
     private val statisticsFragment = StatisticsFragment()
     private lateinit var currentFragment: Fragment
-    private lateinit var nickNameText: TextView
     
     val binding by lazy { ActivityMainBinding.inflate(layoutInflater)}
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,11 +35,9 @@ class MainActivity : AppCompatActivity() {
         getInit()
 
         binding.bnvMain.selectedItemId = R.id.navi_home
-        nickNameText = binding.mainTitleText
 
         binding.bnvMain.itemIconTintList = null
         currentFragment = homeFragment
-
 
         binding.bnvMain.setOnItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
@@ -59,10 +50,10 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun setBundle(hasMission: Boolean){
+    private fun setBundle(hasMission: Boolean, nickName: String?){
         val bundle = Bundle()
+        bundle.putString("nickname", nickName)
         bundle.putBoolean("hasMission", hasMission)
-        Log.d("main has Mission", hasMission.toString())
         homeFragment.arguments = bundle
 
         supportFragmentManager.beginTransaction()
@@ -80,15 +71,13 @@ class MainActivity : AppCompatActivity() {
                     if (response.isSuccessful) {
                         Log.d("initResponse", response.body().toString())
                         val initData: InitResponse.Data = response.body()!!.initData
-                        binding.mainTitleText.text = "${initData.nickname},\n오늘도 나랑 대화하자"
-                        setBundle(initData.has_mission_today)
+                        setBundle(initData.has_mission_today, initData.nickname)
                     } else {
                         Log.d("initResponse", "HTTP 오류")
                     }
                 }
                 override fun onFailure(call: Call<InitResponse>, t: Throwable) {
                     Log.d("initFail", t.toString())
-                    t.printStackTrace()
                 }
             })
         }
@@ -98,17 +87,6 @@ class MainActivity : AppCompatActivity() {
         val transaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.main_frameLayout, fragment)
         transaction.commit()
-
-        if(fragment != homeFragment){
-            binding.topBarLayout.visibility = View.GONE
-        }else{
-            binding.topBarLayout.visibility = View.VISIBLE
-        }
-    }
-
-    private fun showActivity(activity: Activity) {
-        val intent = Intent(this, activity::class.java)
-        startActivity(intent)
     }
 
     fun hideBottomNavigation(state:Boolean){
@@ -116,13 +94,6 @@ class MainActivity : AppCompatActivity() {
             binding.bnvMain.visibility = View.GONE
         }else{
             binding.bnvMain.visibility = View.VISIBLE
-        }
-    }
-
-    fun changeMainTitle(pageNumber: Int, nickName: String?){
-        when(pageNumber){
-            0 -> binding.mainTitleText.setText("${nickName},\n오늘도 나랑 대화하자.")
-            1 -> binding.mainTitleText.setText(R.string.title_mission)
         }
     }
 }
--- app/src/main/java/presentation/home/HomeFragment.kt
+++ app/src/main/java/presentation/home/HomeFragment.kt
@@ -1,13 +1,16 @@
 package presentation.home
 
 import android.content.Intent
+import android.media.SoundPool
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import com.example.tome_aos.R
 import com.example.tome_aos.databinding.FragmentHomeBinding
 import presentation.MainActivity
 import presentation.chat.ChatActivity
@@ -24,17 +27,26 @@ class HomeFragment : Fragment() {
     ): View {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
         val mainActivity = activity as MainActivity
+        val nickName = arguments?.getString("nickname")
         val hasMissionToday = arguments?.getBoolean("hasMission")
+
+        binding.mainTitleText.text = "${nickName},\n오늘도 나랑 대화하자."
+
+        val soundPool = SoundPool.Builder().build()
+        var soundId = soundPool.load(requireContext(), R.raw.mp_bubble, 1)
 
         Log.d("home has mission", hasMissionToday.toString())
 
         binding.toTalkBtn.setOnClickListener {
+            soundPool.play(soundId, 1.0f, 1.0f, 0, 0, 1.0f)
             var intent = Intent(context, ChatActivity::class.java)
             startActivity(intent)
         }
 
         binding.checkMissionBtn.setOnClickListener{
             if(hasMissionToday == true) {
+                soundPool.play(soundId, 1.0f, 1.0f, 0, 0, 1.0f)
+
                 val fragment = MissionFragment()
                 // Fragment 전환을 위한 트랜잭션 시작
                 val transaction = parentFragmentManager.beginTransaction()
@@ -42,12 +54,10 @@ class HomeFragment : Fragment() {
                 transaction.addToBackStack(null);
                 transaction.commit()
 
-                mainActivity.changeMainTitle(1, null)
             }else if(hasMissionToday == false){
                 Toast.makeText(context, "현재 할 수 있는 미션이 없어요!", Toast.LENGTH_SHORT).show()
             }
         }
-
 
         return binding.root
     }
