e3a7f2d5b23e4db80dccdbfd064385844a61fa2c
MinJu Jo <pida6716@gmail.com>
2023-12-06 22:50:38 +0900

    [add] button animation

--- app/src/main/java/presentation/home/HomeFragment.kt
+++ app/src/main/java/presentation/home/HomeFragment.kt
@@ -7,43 +7,55 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
-import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import com.example.tome_aos.R
 import com.example.tome_aos.databinding.FragmentHomeBinding
-import presentation.MainActivity
 import presentation.chat.ChatActivity
 import presentation.mission.MissionFragment
 
 
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
+    private lateinit var chatButton: Button
+    private lateinit var missionButton: Button
+    private lateinit var mainTitle: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val mainActivity = activity as MainActivity
+        binding = FragmentHomeBinding.inflate(inflater, container, false).apply {
+            chatButton = toTalkBtn
+            missionButton = checkMissionBtn
+            mainTitle = mainTitleText
+        }
         val nickName = arguments?.getString("nickname")
         val hasMissionToday = arguments?.getBoolean("hasMission")
 
-        binding.mainTitleText.text = "${nickName},\n오늘도 나랑 대화하자."
+        val chatAnimation = AnimationUtils.loadAnimation(requireContext(),R.anim.bubble_btn);
+        val missionAnimation = AnimationUtils.loadAnimation(requireContext(),R.anim.bubble_btn);
+        chatButton.startAnimation(chatAnimation)
+        missionButton.startAnimation(missionAnimation)
+
+        mainTitle.text = "${nickName},\n오늘도 나랑 대화하자."
 
         val soundPool = SoundPool.Builder().build()
         var soundId = soundPool.load(requireContext(), R.raw.mp_bubble, 1)
 
-        Log.d("home has mission", hasMissionToday.toString())
 
-        binding.toTalkBtn.setOnClickListener {
+        chatButton.setOnClickListener {
             soundPool.play(soundId, 1.0f, 1.0f, 0, 0, 1.0f)
             var intent = Intent(context, ChatActivity::class.java)
             startActivity(intent)
         }
 
-        binding.checkMissionBtn.setOnClickListener{
+        missionButton.setOnClickListener{
             if(hasMissionToday == true) {
                 soundPool.play(soundId, 1.0f, 1.0f, 0, 0, 1.0f)
 
